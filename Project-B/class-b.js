007da8bd7cd4b49d90b3cf702e47c470cd6a2abb
Ashley-Li-Huiyan <hl6630@nyu.edu>
2026-05-07 08:38:12 +0800

    Final Final

--- Project-B/class-b.js
+++ Project-B/class-b.js
@@ -2,7 +2,8 @@ class SoundBlob {
   constructor(bx, by) {
     this.r = []; //radius
     this.R = []; //recorded value from mic
-    this.n = 0; //number of circles
+    this.n = 0; //number of circles and lines
+    this.freq = [] //frequencies for each r
     //push 0 till R.length = n;
     if (this.R.length < this.n) {
       this.R.push(0);
@@ -17,6 +18,11 @@ class SoundBlob {
     this.allowPlay = false;
     this.isdone = false;
     this.lifespan = 100;
+    this.osc = new p5.SinOsc();
+    this.envelope = new p5.Env();
+    this.envelope.setADSR(0.01, 0.2, 0.2, 0.5);
+    this.op = 100;
+    this.index = 0; //for the notes
   }
 
   displayBlob() {
@@ -24,20 +30,19 @@ class SoundBlob {
     translate(this.bx, this.by, this.bz);
     rotate(frameCount * this.speed);
     colorMode(HSB, 100);
-
     beginShape();
     //n is the number of circles in one bubble
     //n = R.length
     for (let i = 0; i < this.n; i++) {
+      let index = floor(map(this.r[i], 0, 100, 0, notes.length - 1));
+      this.freq[i] = notes[index];
       let angle = map(i, 0, this.R.length, 0, 2 * PI);
       let x = 0 + this.r[i] * cos(angle);
       let y = 0 + this.r[i] * sin(angle);
       let z = sin(this.r[i] * 0.1) * 100;
       let H = map(angle, 0, 2 * PI, 0, 100);
       let S = map(angle, 0, 2 * PI, 0, 100);
-
-      //noStroke();
-      stroke(H, S, 100);
+      stroke(H, S, 100, this.op);
       strokeWeight(2);
       noFill();
       curveVertex(x, y, z);
@@ -51,15 +56,12 @@ class SoundBlob {
       let z = sin(this.r[i] * 0.1) * 100;
       let H = map(angle, 0, 2 * PI, 0, 100);
       let S = map(angle, 0, 2 * PI, 0, 100);
-
-      stroke(H, S, 100);
+      stroke(H, S, 100, this.op);
       line(0, 0, 0, x, y, z);
-
       noStroke();
       fill(H, S, 100);
       push();
       translate(x, y, z);
-      //circle(0, 0, 5);
       pop();
       // }
     }
@@ -82,9 +84,26 @@ class SoundBlob {
     }
     //movig back and forth in Z
     this.bz = map(sin(frameCount * this.speedbz), -1, 1, -500, 500);
-    this.lifespan =  this.lifespan - 0.1;
+    this.lifespan = this.lifespan - 0.1;
+    this.op = map(this.lifespan, 100, 0, 100, 0);
     if (this.lifespan < 0) {
       this.isdone = true;
+    }
+  }
+
+  playBlob() {
+    //  this.osc.freq(this.freq[0]);
+    //  this.envelope.play(this.osc, 0, 0.1);
+    console.log(this.freq);
+    if (frameCount % 10 === 0 || frameCount === 1 && this.index < this.n) {
+      this.osc.start();
+      this.osc.freq(this.freq[this.index]);
+      this.envelope.play(this.osc, 0, 0.1);
+      this.index++;  
+   }
+    if(this.index == this.n){
+      this.allowPlay = false;
+      this.index = 0;
     }
   }
 }
--- Project-B/sketch.js
+++ Project-B/sketch.js
@@ -3,30 +3,57 @@ let mic;
 let level = 0;
 let counter = []; //timer for recording
 let index = 0; //counter index
-let sound;
 let allowPlay = []; //allow sound to play
 let d = []; //d between mouse and
 let z = 0; //asign a value
 let handPose;
+let phand;
 let video;
 let hands = [];
-let options = { maxHands: 1, flipped: false };
+let options = { maxHands: 1, flipped: true };
 let rightbutton = false;
 let cam;
+let notes = [
+  261.63, // C4
+  293.66, // D4
+  329.63, // E4
+  349.23, // F4
+  392.0, // G4
+  440.0, // A4
+  493.88, // B4
+  523.25, // C5
+  587.33, // D5
+  659.25, // E5
+  698.46, // F5
+  783.99, // G5
+  880.0, // A5
+  987.77, // B5
+  1046.5, // C6
+  1174.66, // D6
+  1318.51, // E6
+  1396.91, // F6
+  1567.98, // G6
+  1760.0, // A6
+  1975.53, // B6
+  2093.0, // C7
+];
+let isPlaying = false;
+let mysound;
 
 function preload() {
-  sound = loadSound("Sound.mp3");
   handPose = ml5.handPose(options);
+  myFont = loadFont('times/times.TTF');
+  mysound = loadSound("Nocturnes.mp3");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  cam = createCamera(); 
+  cam = createCamera();
   //setting up the mic
   mic = new p5.AudioIn();
   mic.start();
   video = createCapture(VIDEO);
-  video.size(640, 480);
+  video.size(windowWidth, windowHeight);
   video.hide();
   // Start detecting hands from the webcam video
   handPose.detectStart(video, gotHands);
@@ -39,29 +66,54 @@ function gotHands(results) {
 }
 
 function draw() {
+  background(0);
   // Enable orbiting with the mouse.
   orbitControl();
   z = cam.eyeZ;
   //hands
   push();
-  translate(width, 0);
-  scale(-1, 1);
-  image(video, 0, 0, width, height);
+  translate(-width / 2, -height / 2);
+  //text
+  textFont(myFont);
+  fill(255);
+  textSize(16);
+  text("press the right button to create a memory",0,50);
+  text("put your hand near to replay it",0,70);
   // Draw all the tracked hand points
   for (let i = 0; i < hands.length; i++) {
     let hand = hands[i];
-    for (let j = 0; j < hand.keypoints.length; j++) {
-      let keypoint = hand.keypoints[j];
-      fill(0, 255, 0);
-      noStroke();
-      circle(keypoint.x, keypoint.y, 10);
+    phand = hand.keypoints[9];
+    fill(255);
+    beginShape();
+    curveVertex(phand.x, phand.y-10);
+    curveVertex(phand.x-5, phand.y);
+    curveVertex(phand.x-10, phand.y+10);
+    curveVertex(phand.x, phand.y+10);
+    curveVertex(phand.x+10, phand.y+10);
+    curveVertex(phand.x+5, phand.y);
+    endShape(CLOSE);
+    //circle(phand.x, phand.y, 50);
+  }
+  //stars
+  for (let i = 0; i < width; i += 10) {
+    for (let j = 0; j < height; j += 10) {
+      if (noise(i*j+i+j) > 0.8) {
+        push();
+        let cz = map(sin(frameCount * 0.01), -1, 1, -50, 50);
+        let op = map(cos(frameCount * 0.1), -1, 1, 100, 200);
+        translate(i, j, cz);
+        noStroke();
+        fill(255, 255, 255, op);
+        circle(i, j, 5);
+        pop();
+      }
     }
   }
   pop();
   //bubble
   push();
   translate(-width / 2, -height / 2);
-  background(0);
+
   //console.log(counter);
   level = map(mic.getLevel(), 0, 1, 1, 10);
   //each bubble record and display
@@ -69,13 +121,24 @@ function draw() {
     b[i].recordValues();
     b[i].updateValues();
     b[i].displayBlob();
-    d[i] = dist(b[i].bx, b[i].by, b[i].bz, mouseX, mouseY,b[i].bz);
-    //console.log(d[0]);
-    if (d[i] < 100 && sound.isPlaying() == false && b[i].allowPlay == true) {
-      sound.play();
+    if (hands.length > 0) {
+      d[i] = dist(b[i].bx, b[i].by, b[i].bz, phand.x, phand.y, b[i].bz);
+    }else{
+       d[i] = 500;
     }
+
+    console.log(d[i]);
+    //console.log(d[0]);
+    if (d[i] < 100 && b[i].allowPlay) {
+      b[i].playBlob();
+
+    } else if (d[i] > 100 && b[i].allowPlay == false) {
+      b[i].allowPlay = true;
+    }
+
     if (b[i].isdone == true) {
       b.splice(i, 1);
+      index = index - 1;
     }
   } //b for loop ends
 
@@ -88,6 +151,12 @@ function draw() {
     counter[index]++; //n of the current bubble ++
   } //mouse is pressed ends
   pop();
+
+  if (keyIsPressed){
+    if(key === "a" || key === "A" && mysound.isPlaying()==false){
+      mysound.play();
+    }
+  }
 } // draw ends
 
 //each time I click, push one bubble and its counter
@@ -95,7 +164,7 @@ function mousePressed() {
   if (mouseButton == RIGHT) {
     b.push(new SoundBlob(mouseX, mouseY));
     counter.push(0);
-    console.log("mouse pressed");
+    //console.log("mouse pressed");
     rightbutton = true;
   }
 }
@@ -104,7 +173,7 @@ function mouseReleased() {
   if (rightbutton == true && b.length > 0) {
     b[index].allowPlay = true;
     index++;
-    console.log("mouse released");
+    // console.log("mouse released");
     rightbutton = false;
   }
 }
